6c18b5ab53a9f31007636efc024ad698fc9f3ba1
nefiblanco <bla22061@byui.edu>
2023-01-10 17:43:05 -0500

    Creating the guessing game.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string play = "";
+
+        do{
+        
+            Random randomNumber = new Random();
+            int createdNumber = randomNumber.Next(1, 101);
+
+            int input = 0;
+            int count = 0;
+            while (input != createdNumber)
+            {
+                Console.Write("What is your guess? ");
+                input = int.Parse(Console.ReadLine());
+
+                if (createdNumber > input)
+                {
+                    Console.WriteLine("Higher");
+                    count ++;
+                }
+                else if (createdNumber < input)
+                {
+                    Console.WriteLine("Lower");
+                    count ++;
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {count} attempts!");
+                }
+            }
+
+            Console.Write("Would you like to play again?");
+            play = Console.ReadLine();
+
+        }while(play == "yes");
     }
 }

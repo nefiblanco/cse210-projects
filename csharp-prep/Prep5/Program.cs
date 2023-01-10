1e94b0511aa889b1d3ba65775c6a5cc0d12fbc39
nefiblanco <bla22061@byui.edu>
2023-01-10 18:50:13 -0500

    Creating the functions program

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squarednumber = SquareNumber(number);
+        DisplayResult(name, squarednumber);
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+       Console.WriteLine("Please enter your name:");
+       string name = Console.ReadLine();
+       return (name);
+    } 
+    static int PromptUserNumber()
+    {
+       Console.WriteLine("Please enter your favorite number:");
+       int number = int.Parse(Console.ReadLine());
+       return (number);
+    }
+    static int SquareNumber(int number)
+    {
+        int squarednumber = number * number;
+        return (squarednumber);
+    }
+    static void DisplayResult(string name, int squarednumber)
+    {
+       Console.WriteLine($"{name}, the square of your number is {squarednumber}");
     }
 }

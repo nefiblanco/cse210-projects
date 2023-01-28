549ec61c495b110d5834c3b955e38458859ab3b0
nefiblanco <bla22061@byui.edu>
2023-01-28 14:28:54 -0500

    Adding more prompts

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -52,7 +52,12 @@ class Program
             "What was the best part of my day?",
             "How did I see the hand of the Lord in my life today?",
             "What was the strongest emotion I felt today?",
-            "If I had one thing I could do over today, what would it be?"
+            "If I had one thing I could do over today, what would it be?",
+            "What scripture did I read today?",
+            "What changes can I make to become a better person?",
+            "Did I do something good for someone else?",
+            "Did I help out at home today?",
+            "What is my goal for tomorrow?",
         };
         Random random = new Random();
         int index = random.Next(prompts.Count);
